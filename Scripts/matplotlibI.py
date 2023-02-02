45ef15167cd4e85bd73225a2d8c2de8c0da0e8ea
FelipeWcosta <83873110+FelipeWcosta@users.noreply.github.com>
2023-02-02 15:02:27 -0300

    Update matplotlibI.py

--- Scripts/matplotlibI.py
+++ Scripts/matplotlibI.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 x = [0, 1, 2, 3, 4, 5]
 y = [0, 2, 4, 6, 8, 10]
 
-plt.plot(x, y)
+plt.plot(x, y, label = '2x', color = 'green', linewidth = 1, linestyle = 'dashdot', marker = 'h', markersize = 10, markeredgecolor = 'blue')
 plt.title('Primeiro Gráfico', fontdict = {'fontname': 'Fira Code', 'fontsize': 20})
 plt.xlabel('Eixo x')
 plt.ylabel('Eixo y')
 plt.xticks([0, 1, 2, 3, 4, 5])
 plt.yticks([0, 2, 4, 6, 8, 10, 12])
+plt.legend()
 plt.show()
