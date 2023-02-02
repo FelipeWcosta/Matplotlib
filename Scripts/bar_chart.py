85a317f378ab6d83a624586058b8366f198bc21f
FelipeWcosta <83873110+FelipeWcosta@users.noreply.github.com>
2023-02-02 17:25:24 -0300

    Update bar_chart.py

--- Scripts/bar_chart.py
+++ Scripts/bar_chart.py
@@ -4,5 +4,13 @@ import matplotlib.pyplot as plt
 labels = ['a', 'b', 'c', 'd']
 values = [2, 3, 4, 6]
 
-plt.bar(values, labels)
+bars = plt.bar(labels, values)
+
+
+bars[0].set_hatch('/')
+bars[1].set_hatch('o')
+bars[2].set_hatch('*')
+
+#plt.figure(figsize=(6, 4))
+
 plt.show()
