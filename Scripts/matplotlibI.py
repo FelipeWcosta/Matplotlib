f42702e445b96fbd94c6ae4bbc5b26b440adebfb
FelipeWcosta <83873110+FelipeWcosta@users.noreply.github.com>
2023-02-02 15:12:27 -0300

    Update matplotlibI.py

--- Scripts/matplotlibI.py
+++ Scripts/matplotlibI.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 x = [0, 1, 2, 3, 4, 5]
 y = [0, 2, 4, 6, 8, 10]
 
-plt.plot(x, y, label = '2x', color = 'green', linewidth = 1, linestyle = 'dashdot', marker = 'h', markersize = 10, markeredgecolor = 'blue')
+x2 = np.arange(0, 4, 0.5)
+plt.plot(x2[:5], x2[:5]**2, 'm--')
+plt.plot(x2[4:], x2[4:]**2, 'm', label = '$x^2$')
+
+plt.plot(x, y, label = '$2x$', color = 'green', linewidth = 1, linestyle = 'dashdot', marker = 'h', markersize = 10, markeredgecolor = 'blue')
 plt.title('Primeiro Gráfico', fontdict = {'fontname': 'Fira Code', 'fontsize': 20})
 plt.xlabel('Eixo x')
 plt.ylabel('Eixo y')
