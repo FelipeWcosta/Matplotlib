8226db7b5e0aced860bd9cff773fc1ec15557bc8
FelipeWcosta <83873110+FelipeWcosta@users.noreply.github.com>
2023-02-02 14:44:44 -0300

    Update matplotlibI.py

--- Scripts/matplotlibI.py
+++ Scripts/matplotlibI.py
@@ -5,7 +5,7 @@ x = [0, 1, 2, 3, 4, 5]
 y = [0, 2, 4, 6, 8, 10]
 
 plt.plot(x, y)
-plt.title('Primeiro Gráfico', fontdict = {'fontname': 'Times New Roman', 'fontsize': 20})
+plt.title('Primeiro Gráfico', fontdict = {'fontname': 'Fira Code', 'fontsize': 20})
 plt.xlabel('Eixo x')
 plt.ylabel('Eixo y')
 plt.xticks([0, 1, 2, 3, 4, 5])
